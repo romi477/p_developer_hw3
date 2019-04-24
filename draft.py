fe1b5a9a7dfbe86d266b5f960614b3bdde32522d
romi477 <romi4240101@gmail.com>
2019-04-24 17:55:12 +0300

    ...

--- draft.py
+++ draft.py
@@ -34,9 +34,52 @@ GENDERS = {
     FEMALE: "female",
 }
 
-r = {
+r1 = {
     "account": "horns&hoofs",
     "login": "admin",
+    "method": "clients_interests",
+    "token": "d3573aff1555cd67dccf21b95fe8c4dc8732f33fd4e32461b7fe6a71d83c947688515e36774c00fb630b039fe2223c991f045f13f2",
+    "arguments": {"client_ids": [1, 2, 3, 4], "date": "20.07.2017"}
+}
+
+r2 = {
+    "account2": "horns&hoofs",
+    "login": "admin",
+    "method": "clients_interests",
+    "token": "d3573aff1555cd67dccf21b95fe8c4dc8732f33fd4e32461b7fe6a71d83c947688515e36774c00fb630b039fe2223c991f045f13f2",
+    "arguments": {"client_ids": [1, 2, 3, 4], "date": "20.07.2017"}
+}
+
+
+r3 = {
+    "account": "horns&hoofs",
+    "login": "",
+    "method": "clients_interests",
+    "token": "d3573aff1555cd67dccf21b95fe8c4dc8732f33fd4e32461b7fe6a71d83c947688515e36774c00fb630b039fe2223c991f045f13f2",
+    "arguments": {"client_ids": [1, 2, 3, 4], "date": "20.07.2017"}
+}
+
+r4 = {
+    "account": "horns&hoofs",
+    "login2": "",
+    "method": "",
+    "token": "d3573aff1555cd67dccf21b95fe8c4dc8732f33fd4e32461b7fe6a71d83c947688515e36774c00fb630b039fe2223c991f045f13f2",
+    "arguments": {"client_ids": [1, 2, 3, 4], "date": "20.07.2017"}
+}
+
+
+r5 = {
+    "account": "horns&hoofs",
+    "login": "",
+    "method2": "clients_interests",
+    "token": "d3573aff1555cd67dccf21b95fe8c4dc8732f33fd4e32461b7fe6a71d83c947688515e36774c00fb630b039fe2223c991f045f13f2",
+    "arguments": {"client_ids": [1, 2, 3, 4], "date": "20.07.2017"}
+}
+
+
+r6 = {
+    "account": "horns&hoofs",
+    "login": "",
     "method": "clients_interests",
     "token": "d3573aff1555cd67dccf21b95fe8c4dc8732f33fd4e32461b7fe6a71d83c947688515e36774c00fb630b039fe2223c991f045f13f2",
     "arguments": {"client_ids": [1, 2, 3, 4], "date": "20.07.2017"}
@@ -46,20 +89,19 @@ class Store:
     def __init__(self, cls):
         self.cls = cls
 
-class FieldsContainer():
+
+class FieldsContainer:
     def __init__(self, fields):
         self._fields = fields
 
-    def __repr__(self):
-        return '\n'.join([f'{k} - {v}' for k, v in self._fields.items()])
-
-    def get_field(self, key):
-        return self._fields.get(key, 'Not found field')
-
-    def has_field(self, field):
-        return field in self._fields
-
-
+    # def __repr__(self):
+    #     return '\n'.join([f'{k} - {v}' for k, v in self._fields.items()])
+    #
+    # def get_field(self, key):
+    #     return self._fields.get(key, 'Not found field')
+    #
+    # def has_field(self, field):
+    #     return field in self._fields
 
 
 
@@ -71,10 +113,6 @@ class Field:
 
     def is_valid_field(self, value):
         print('Init <Field> validate')
-        if self.required and not value:
-            raise ValueError('This field is required.')
-        if not self.nullable and value:
-            raise ValueError('This field cannot be empty.')
 
 
 class CharField(Field):
@@ -113,41 +151,41 @@ class ClientIDsField(Field):
 
 
 class RequestMeta(type):
-    def __new__(mcls, name, bases, attrs):
-        fields = {}
+    def __new__(cls, name, bases, attrs):
+        field_instances = {}
         other_attrs = {}
         for key, value in attrs.items():
             if isinstance(value, Field):
-                fields[key] = value
+                field_instances[key] = value
             else:
                 other_attrs[key] = value
-        new_cls = super().__new__(mcls, name, bases, other_attrs)
+        new_cls = super().__new__(cls, name, bases, other_attrs)
         # new_cls.store = Store(new_cls)
-        new_cls._fields_data = FieldsContainer(fields)
+        new_cls._fields_container = FieldsContainer(field_instances)
         return new_cls
 
 
 class Request(metaclass=RequestMeta):
-    def __init__(self, params={}):
+    def __init__(self, params):
         self.errors = {}
         self.params = params
+        self._clean_fields = []
 
-        for field, value in params.items():
-            if self._fields_data.has_field(field):
-                setattr(self, field, value)
-
-        #     if key in self._fields_data and self._fields_data.get(key).is_valid_field(value):
-        #         setattr(self, key, value)
-        #     else:
-        #         self.errors[key] = f'<{value}> is not valid.'
-
-
-
-
-        # for item in params:
-        #     if item in self.fields_data:
-        #         setattr(self, item, params.get(item))
-
+        for field_name, field_instance in self._fields_container._fields.items():
+            params_value = self.params.get(field_name)
+            if field_instance.required and field_name not in self.params:
+                print(f'{field_name} - required BAD!')
+                print('skip checking nullable')
+                self.errors[field_name] = 'This field is required.'
+            else:
+                print(f'{field_name} - required OK!')
+                if not field_instance.nullable and not params_value:
+                    print(f'{field_name} - nullable BAD!')
+                    self.errors[field_name] = 'This field cannot be empty.'
+                else:
+                    print(f'{field_name} - nullable OK!')
+                    setattr(self, field_name, field_instance)
+                    self._clean_fields.append(field_name)
 
 
     def is_valid(self):
@@ -184,7 +222,6 @@ class MethodRequest(Request):
     arguments = ArgumentsField(required=True, nullable=True)
     method = CharField(required=True, nullable=False)
 
-
     @property
     def is_admin(self):
         return self.login == ADMIN_LOGIN
@@ -200,8 +237,10 @@ def check_auth(request):
     return False
 
 
-def execute_request(instance, request):
-    return 10, 20
+def execute_request(model, request):
+    data_instance = model(request)
+    if not data_instance.is_valid():
+        return data_instance.errors, INVALID_REQUEST
 
 
 def method_handler(request):
